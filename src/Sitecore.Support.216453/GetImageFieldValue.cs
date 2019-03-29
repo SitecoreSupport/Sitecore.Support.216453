2710ed770d86c01722f9a6d48706d2821d5a5d19
DK\ELB <elb@sitecore.net>
2019-03-29 13:20:31 +0200

    no message

---
+++ src/Sitecore.Support.216453/GetImageFieldValue.cs
@@ -0,0 +1,31 @@
+﻿namespace Sitecore.Support.Pipelines.RenderField
+{
+    using Sitecore.Xml.Xsl;
+    using System;
+
+    public class GetImageFieldValue
+    {
+        protected virtual ImageRenderer CreateRenderer() =>
+            new ImageRenderer();
+
+        public void Process(Sitecore.Pipelines.RenderField.RenderFieldArgs args)
+        {
+            if (args.FieldTypeKey == "image")
+            {
+                ImageRenderer renderer = this.CreateRenderer();
+                renderer.Item = args.Item;
+                renderer.FieldName = args.FieldName;
+                renderer.FieldValue = args.FieldValue;
+                renderer.Parameters = args.Parameters;
+                args.WebEditParameters.AddRange(args.Parameters);
+                renderer.Parameters.Add("la", args.Item.Language.Name);
+                RenderFieldResult result = renderer.Render();
+                args.Result.FirstPart = result.FirstPart;
+                args.Result.LastPart = result.LastPart;
+                args.DisableWebEditContentEditing = true;
+                args.DisableWebEditFieldWrapping = true;
+                args.WebEditClick = "return Sitecore.WebEdit.editControl($JavascriptParameters, 'webedit:chooseimage')";
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.216453/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.216453")]
+[assembly: AssemblyProduct("Sitecore.Support.216453")]
+[assembly: ComVisible(false)]
